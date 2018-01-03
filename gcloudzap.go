5b28e2711cfd4885e62fa2e573de7f1a7e4a4f34
Jon Staryuk <jon@staryuk.com>
2018-01-03 12:52:36 -0800

    Initial commit

---
+++ gcloudzap.go
@@ -0,0 +1,233 @@
+/*
+Package gcloudzap provides a zap logger that forwards entries to the Google
+Stackdriver Logging service as structured payloads.
+
+All zap.Logger instances created with this package are safe for concurrent
+use.
+
+Writes are buffered and asynchronous. These benchmarks, on a MacBook Pro 2.4
+GHz Core i5, are a loose approximation of latencies on the critical path for
+the zapcore.Core implementation provided by this package.
+
+	$ go test -bench . github.com/jonstaryuk/gcloudzap
+	goos: darwin
+	goarch: amd64
+	pkg: github.com/jonstaryuk/gcloudzap
+	BenchmarkCoreClone-4   	 2000000	       607 ns/op
+	BenchmarkCoreWrite-4   	 1000000	      2811 ns/op
+
+
+Zap docs: https://godoc.org/go.uber.org/zap
+Stackdriver Logging docs: https://cloud.google.com/logging/docs/
+
+*/
+package gcloudzap // import "github.com/jonstaryuk/gcloudzap"
+
+import (
+	"context"
+	"fmt"
+
+	gcl "cloud.google.com/go/logging"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func newClient(projectID string) (*gcl.Client, error) {
+	if projectID == "" {
+		return nil, newError("the provided projectID is empty")
+	}
+
+	return gcl.NewClient(context.Background(), projectID)
+}
+
+// NewDevelopment builds a development Logger that writes DebugLevel and above
+// logs to standard error in a human-friendly format, as well as to
+// Stackdriver using Application Default Credentials.
+func NewDevelopment(projectID string, logID string) (*zap.Logger, error) {
+	if logID == "" {
+		return nil, fmt.Errorf("the provided logID is empty")
+	}
+
+	client, err := newClient(projectID)
+	if err != nil {
+		return nil, newError("creating Google Logging client: %v", err)
+	}
+
+	return New(zap.NewDevelopmentConfig(), client, logID)
+}
+
+// NewProduction builds a production Logger that writes InfoLevel and above
+// logs to standard error as JSON, as well as to Stackdriver using Application
+// Default Credentials.
+func NewProduction(projectID string, logID string) (*zap.Logger, error) {
+	if logID == "" {
+		return nil, fmt.Errorf("the provided logID is empty")
+	}
+
+	client, err := newClient(projectID)
+	if err != nil {
+		return nil, newError("creating Google Logging client: %v", err)
+	}
+
+	return New(zap.NewProductionConfig(), client, logID)
+}
+
+// New creates a new zap.Logger which will write entries to Stackdriver in
+// addition to the destination specified by the provided zap configuration.
+func New(cfg zap.Config, client *gcl.Client, logID string, opts ...zap.Option) (*zap.Logger, error) {
+	zl, err := cfg.Build(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	if client == nil {
+		return nil, fmt.Errorf("The provided GCL client is nil")
+	}
+
+	tee := Tee(zl.Core(), client, logID)
+	return zap.New(tee, opts...), nil
+}
+
+// A Core implements zapcore.Core and writes entries to a Logger from the
+// Google Cloud package.
+//
+// It's safe for concurrent use by multiple goroutines as long as it's not
+// mutated after first use.
+type Core struct {
+	// Logger is a logging.Logger instance from the Google Cloud Platform Go
+	// library.
+	Logger GoogleCloudLogger
+
+	// Provide your own mapping of zapcore's Levels to Google's Severities, or
+	// use DefaultSeverityMapping. All of the Core's children will default to
+	// using this map.
+	//
+	// This must not be mutated after the Core's first use.
+	SeverityMapping map[zapcore.Level]gcl.Severity
+
+	// MinLevel is the minimum level for a log entry to be written.
+	MinLevel zapcore.Level
+
+	// fields should be built once and never mutated again.
+	fields map[string]interface{}
+}
+
+// Tee returns a zapcore.Core that writes entries to both the provided core
+// and to Stackdriver using the provided client and log ID.
+//
+// For fields to be written to Stackdriver, you must use the With() method on
+// the returned Core rathern than just on zc. (This function has no way of
+// knowing about fields that already exist on zc. They will be preserved when
+// writing to zc's existing destination, but not to Stackdriver.)
+func Tee(zc zapcore.Core, client *gcl.Client, gclLogID string) zapcore.Core {
+	gc := &Core{
+		Logger:          client.Logger(gclLogID),
+		SeverityMapping: DefaultSeverityMapping,
+	}
+
+	for l := zapcore.DebugLevel; l <= zapcore.FatalLevel; l++ {
+		if zc.Enabled(l) {
+			gc.MinLevel = l
+			break
+		}
+	}
+
+	return zapcore.NewTee(zc, gc)
+}
+
+// Enabled implements zapcore.Core. It is an alias to c.LevelEnabled.
+func (c *Core) Enabled(l zapcore.Level) bool {
+	return l >= c.MinLevel
+}
+
+// With implements zapcore.Core.
+func (c *Core) With(newFields []zapcore.Field) zapcore.Core {
+	return &Core{
+		Logger:          c.Logger,
+		SeverityMapping: c.SeverityMapping,
+		MinLevel:        c.MinLevel,
+		fields:          clone(c.fields, newFields),
+	}
+}
+
+// Check implements zapcore.Core.
+func (c *Core) Check(e zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if c.Enabled(e.Level) {
+		return ce.AddCore(e, c)
+	}
+	return ce
+}
+
+// Write implements zapcore.Core. It writes a log entry to Stackdriver.
+//
+// The "logger", "msg", "caller", and "stack" fields on the payload are
+// populated from their respective values on the zapcore.Entry.
+func (c *Core) Write(ze zapcore.Entry, newFields []zapcore.Field) error {
+	severity, specified := c.SeverityMapping[ze.Level]
+	if !specified {
+		severity = gcl.Default
+	}
+
+	payload := clone(c.fields, newFields)
+
+	payload["logger"] = ze.LoggerName
+	payload["msg"] = ze.Message
+	payload["caller"] = ze.Caller.String()
+	payload["stack"] = ze.Stack
+
+	c.Logger.Log(gcl.Entry{
+		Timestamp: ze.Time,
+		Severity:  severity,
+		Payload:   payload,
+	})
+
+	return nil
+}
+
+// Sync implements zapcore.Core. It flushes the Core's Logger instance.
+func (c *Core) Sync() error {
+	if err := c.Logger.Flush(); err != nil {
+		return newError("flushing Google Cloud logger: %v", err)
+	}
+	return nil
+}
+
+// DefaultSeverityMapping is the default mapping of zap's Levels to Google's
+// Severities.
+var DefaultSeverityMapping = map[zapcore.Level]gcl.Severity{
+	zapcore.DebugLevel:  gcl.Debug,
+	zapcore.InfoLevel:   gcl.Info,
+	zapcore.WarnLevel:   gcl.Warning,
+	zapcore.ErrorLevel:  gcl.Error,
+	zapcore.DPanicLevel: gcl.Critical,
+	zapcore.PanicLevel:  gcl.Critical,
+	zapcore.FatalLevel:  gcl.Critical,
+}
+
+// clone creates a new field map without mutating the original.
+func clone(orig map[string]interface{}, newFields []zapcore.Field) map[string]interface{} {
+	clone := make(map[string]interface{})
+
+	for k, v := range orig {
+		clone[k] = v
+	}
+
+	for _, f := range newFields {
+		clone[f.Key] = f.Interface
+	}
+
+	return clone
+}
+
+const packageName = "gcloudzap"
+
+// newError calls fmt.Errorf() and prefixes the error with the packageName.
+func newError(format string, args ...interface{}) error {
+	return fmt.Errorf(packageName+": "+format, args)
+}
+
+// GoogleCloudLogger encapsulates the important methods of gcl.Logger
+type GoogleCloudLogger interface {
+	Flush() error
+	Log(e gcl.Entry)
+}
---
+++ gcloudzap_test.go
@@ -0,0 +1,170 @@
+package gcloudzap
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	gcl "cloud.google.com/go/logging"
+	"github.com/google/go-cmp/cmp"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestCoreFields(t *testing.T) {
+	c1 := &Core{}
+	c2 := c1.With([]zapcore.Field{
+		{Key: "foo", Interface: "bar"},
+	}).(*Core)
+	c3 := c2.With([]zapcore.Field{
+		{Key: "baz", Interface: "qux"},
+	}).(*Core)
+
+	if len(c1.fields) != 0 {
+		t.Error("c1 should not have any fields")
+	}
+
+	expected := map[string]interface{}{"foo": "bar"}
+	if diff := cmp.Diff(expected, c2.fields); diff != "" {
+		t.Error(diff)
+	}
+
+	expected = map[string]interface{}{"foo": "bar", "baz": "qux"}
+	if diff := cmp.Diff(expected, c3.fields); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestCoreWrite(t *testing.T) {
+	l := &testLogger{}
+	ts := time.Now()
+
+	c1 := &Core{Logger: l, SeverityMapping: DefaultSeverityMapping}
+	c2 := c1.With([]zapcore.Field{
+		{Key: "foo", Interface: "bar"},
+	})
+
+	e := zapcore.Entry{
+		Message:    "hello",
+		LoggerName: "test",
+		Level:      zapcore.WarnLevel,
+		Time:       ts,
+	}
+	fields := []zapcore.Field{{Key: "baz", Interface: "qux"}}
+	if err := c2.Write(e, fields); err != nil {
+		t.Error(err)
+	}
+	expected := []gcl.Entry{
+		{
+			Timestamp: ts,
+			Severity:  gcl.Warning,
+			Payload: map[string]interface{}{
+				"msg":    "hello",
+				"logger": "test",
+				"caller": "undefined",
+				"stack":  "",
+				"foo":    "bar",
+				"baz":    "qux",
+			},
+		},
+	}
+	if diff := cmp.Diff(expected, l.entries); diff != "" {
+		t.Error(diff)
+	}
+
+	fields = []zapcore.Field{{Key: "asdf", Interface: "asdf"}}
+	if err := c2.Write(e, fields); err != nil {
+		t.Error(err)
+	}
+	expected = append(expected, gcl.Entry{
+		Timestamp: ts,
+		Severity:  gcl.Warning,
+		Payload: map[string]interface{}{
+			"msg":    "hello",
+			"logger": "test",
+			"caller": "undefined",
+			"stack":  "",
+			"foo":    "bar",
+			"asdf":   "asdf",
+		},
+	})
+	if diff := cmp.Diff(expected, l.entries); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestConcurrentCoreWrite(t *testing.T) {
+	l := &testLogger{}
+	c := &Core{Logger: l}
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		index := i // capture loop variable
+		go func() {
+			fields := []zapcore.Field{{Key: "i", Interface: index}}
+			if err := c.Write(zapcore.Entry{}, fields); err != nil {
+				t.Error(err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	found := map[int]bool{}
+	for _, e := range l.entries {
+		i := e.Payload.(map[string]interface{})["i"].(int)
+		found[i] = true
+	}
+
+	if len(found) != 100 {
+		t.Error(found)
+	}
+}
+
+func BenchmarkCoreClone(b *testing.B) {
+	c := &Core{}
+	for i := 0; i < b.N; i++ {
+		c.With([]zapcore.Field{
+			{Key: "i", Interface: i},
+			{Key: "foo", Interface: "bar"},
+			{Key: "bar", Interface: "baz"},
+			{Key: "longstring", Interface: "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"},
+			{Key: "longstring2", Interface: "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"},
+			{Key: "longstring3", Interface: "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"},
+		})
+	}
+}
+
+type testLogger struct {
+	entries []gcl.Entry
+	mu      sync.Mutex
+}
+
+func (t *testLogger) Flush() error { return nil }
+
+func (t *testLogger) Log(e gcl.Entry) {
+	// simulate adding entries to a buffer
+	t.mu.Lock()
+	t.entries = append(t.entries, e)
+	t.mu.Unlock()
+}
+
+func BenchmarkCoreWrite(b *testing.B) {
+	c := &Core{
+		Logger: &testLogger{},
+		fields: map[string]interface{}{
+			"foo":         "bar",
+			"longstring":  "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf",
+			"longstring2": "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf",
+			"longstring3": "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf",
+		},
+	}
+	for i := 0; i < b.N; i++ {
+		e := zapcore.Entry{
+			Message:    "hello",
+			LoggerName: "benchmark",
+			Level:      zapcore.WarnLevel,
+		}
+		c.Write(e, []zapcore.Field{})
+	}
+}
